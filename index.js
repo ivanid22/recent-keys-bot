ffd4a1507b72d4beb8eabac0d611b852c9592b01
Ivan Diaz <ivanid22@gmail.com>
2022-05-15 20:30:24 -0300

    Add http listener so Heroku wont kill the app after 60 seconds

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 require('dotenv').config();
+const express = require('express');
 const bot = require('./bot/setup');
+
+// 
+const app = express();
+
+app.get('/', (req, res) => { res.end() });
+
+app.listen(process.env.PORT || 5000, () => { console.log(`Listening on port ${process.env.PORT ? process.env.PORT : '5000'}`) });
 
 bot.login(process.env.BOT_TOKEN);
